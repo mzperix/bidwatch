f70e7339bbdcf8ee837d97dd54153973816a5052
Priscilla <prikoo@gmail.com>
2019-02-17 04:18:52 -0800

    TEXT

--- info_card.R
+++ info_card.R
@@ -3,9 +3,9 @@ library(shiny)
 
 info_card <- function(entity){
   return(
-    HTML(paste(c("<div class='entityName'>",entity[['name']],"</div>",
-                 entity[['title']],
-                 entity[['description']]),
-               sep="<br/>"))
+    HTML(paste('<div class="entity"><p class="entityName">',
+                  entity[['name']],'</p><p class="entityTitle">', 
+                  entity[['title']],'</p><p class="entityDescription">',
+                  entity[['description']]),'</p></div>')
     )
 }
