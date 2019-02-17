eb6e53e1a2d5a76421b46152c23adf1dad727e2d
Justin <justinolshavsky@gmail.com>
2019-02-17 02:40:45 -0800

    Update numContracts.R

--- numContracts.R
+++ numContracts.R
@@ -4,13 +4,12 @@ library(magrittr)
 library(readr)
 library(stringi)
 
-# numContracts <- function(cName,data){
+numContracts <- function(cName,data){
  
-  cName <- "INTERNATIONAL ROAD DYNAMICS INC"
-  data <- read_csv("GitHub/bidwatch/data/all_contracts_test.csv")
+  # cName <- "INTERNATIONAL ROAD DYNAMICS INC"
+  # data <- read_csv("GitHub/bidwatch/data/all_contracts_test.csv")
 
   matrix1 <- data$recipient_name
-  contractsNumber <- length(grep(cName, data))
-#   # return(contractsNumber)  
-#    
-# # }
+  contractsNumber <- length(grep(cName, matrix1))
+ return(contractsNumber)  
+ }
